31e42a8564e328267ea0a4e1d0937f4855d4a361
fasidOnGit <fasidmpm@gmail.com>
2020-02-18 11:39:05 +0530

    Marbles

---
+++ marbles.ts
@@ -0,0 +1,23 @@
+/**
+ * Marble Syntax
+ * -  Time: 10 "frames" of time passage
+ * | Data flow complete!
+ * # Error
+ * a a value emitted by the observable
+ * () sync groupings
+ * ^ subscription point (hot observable only!)
+ */
+
+/**
+ * - Represents 10 frames.
+ * A frame is 1ms
+ */
+
+/**
+ * Hot Observables - Are creating values before the subscription has started. For Eg: Mouse click.
+ * A subject that behaves as though it's already running when the subscription begins!
+ * 
+ * 
+ * Cold Observables - A subject that start "running" when the subscription begins.
+ * Much more predictive.!
+ */
